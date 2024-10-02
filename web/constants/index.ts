bf22f20d61bce42c14bc2bf3b3923b62f85ec2da
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2024-10-02 02:41:04 -0400

    chore: before putting back order book

--- web/constants/index.ts
+++ web/constants/index.ts
@@ -15,14 +15,16 @@ export const NETWORK_URL = clusterApiUrl(CURRENT_NETWORK);
 
 // Your contract program ID
 // export const CONTRACT_PROGRAM_ID: PublicKey = new PublicKey('FobNvbQsK5BAniZC2oJhXakjcPiArpsthTGDnX9eHDVY');
-export const CONTRACT_PROGRAM_ID: PublicKey = new PublicKey('3G5qWYYUBdc79hYTZqQ2TX2cvFxVgbhuSFcm7APispCw');
+export const VAULT_CONTRACT_PROGRAM_ID: PublicKey = new PublicKey('3G5qWYYUBdc79hYTZqQ2TX2cvFxVgbhuSFcm7APispCw');
+export const PERP_CONTRACT_PROGRAM_ID: PublicKey = new PublicKey('A7Lpa8Sg8cb9ZdHG7t1TJGyy5NsvbXxgkcR6P8r4nrig');
+
 //A7Lpa8Sg8cb9ZdHG7t1TJGyy5NsvbXxgkcR6P8r4nrig
 
 // Collateral token mint
 export const COLLATERAL_MINT: PublicKey = new PublicKey('WQgfFKMeNyuANha2hsuCxgP36Qwfo2L8PGReAD1jBPp');
 
 // Maximum number of positions
-export const MAX_POSITIONS: number = 10;
+export const MAX_POSITIONS: number = 10; // dont need anymore
 
 // You can keep the localhost URL for local development if needed
 export const LOCALHOST_URL = 'http://127.0.0.1:8899';
